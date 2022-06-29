2c6b640e9443f4048f5eb8e4f7e63cf0d86b4d83
dipalidatir-demo <dipalidatir1997@gmail.com>
2022-06-29 13:21:51 +0530

    internControllerValid done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,41 +1,40 @@
 const collegeModel = require("../model/collegeModel");
-const {isValidName,isValid } = require("../validation/validator");
+const { isValidName, isValid } = require("../validation/validator");
 
-const createCollege = async function (req,res){
+const createCollege = async function (req, res) {
 
-try {
-    let data= req.body
+    try {
+        let data = req.body
 
-    if (Object.keys(data).length == 0) {
-        return res.status(400).send({status: false, msg: "Body should not be empty" })
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ status: false, msg: "Body should not be empty" })
+        }
+        if (!("name" in data) || !("fullName" in data) || !("logoLink" in data))
+            return res.status(400).send({ status: false, msg: "name, fullname,logoLink are required" })
+
+        // if (!isValid(data.name))
+        // return res.status(400).send({ status: false, msg: "The name Attributes should not be empty" })
+        // if (!isValidName(data.name))
+        // return res.status(400).send({ status: false, msg: "Pls Enter Valid Name" })
+
+        if (!isValid(data.name))
+            return res.status(400).send({ status: false, msg: "The name Attributes should not be empty" })
+        if (!isValidName(data.name))
+            return res.status(400).send({ status: false, msg: "Pls Enter Valid Name" })
+
+
+        let checkunique = await authorModel.findOne({ name: req.body.name })
+        if (checkunique) return res.status(400).send({ status: false, msg: "This name Already Exists Pls Use Another" })
+
+
+        let savedData = await collegeModel.create(data);
+        res.status(201).send({ status: true, msg: "College Created Successfully", data: savedData });
+
     }
-    if (!("name" in data) || !("fullName" in data) || !("logoLink" in data)) 
-    return res.status(400).send({status:false, msg: "name, fullname,logoLink are required" })
-    
-    if (!isValid(data.name))
-    return res.status(400).send({ status: false, msg: "The name Attributes should not be empty" })
-    if (!isValidName(data.name))
-    return res.status(400).send({ status: false, msg: "Pls Enter Valid Name" })
 
-    if (!isValid(data.name))
-    return res.status(400).send({ status: false, msg: "The name Attributes should not be empty" })
-    if (!isValidName(data.name))
-    return res.status(400).send({ status: false, msg: "Pls Enter Valid Name" })
-
-    let checkunique= await authorModel.findOne({name:req.body.name}) 
-    if (checkunique) return res.status(400).send({status:false,msg:"This name Already Exists Pls Use Another"})
-
-
-    let savedData = await collegeModel.create(data);
-        res.status(201).send({ status: true,msg:"College Created Successfully", data: savedData });
-
-
-
-}
-
-catch (error) {
-    return res.status(500).send({ status: false, msg: error.message })
-}
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message })
+    }
 
 }
 module.exports.createCollege = createCollege
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 
 //....................Controllers
-const collegeController= require("../controller/collegeController")
-const internController= require("../controller/interncontroller")
+const collegeController = require("../controller/collegeController")
+const internController = require("../controller/interncontroller")
 
 
 
 //....................Create functionup/colleges
-router.post("/functionup/colleges", collegeController.createCollege)
+// router.post("/functionup/colleges", collegeController.createCollege)
 
 //....................Create  /functionup/interns
-// router.post("/functionup/interns", internController.createIntern)
+router.post("/functionup/interns", internController.createIntern)
 
 //...................Get List Of functionup/colleges
 //  router.get("/functionup/collegeDetails", internController.getAllInterns)
